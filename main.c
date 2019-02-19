4e1e7e87ab33f838b5f138fab14a89a9adf2a45b
yamori813 <yamori813@yahoo.co.jp>
2019-02-19 17:43:01 +0900

    add small page support

--- main.c
+++ main.c
@@ -33,6 +33,25 @@ unsigned int mmu_section ( unsigned int vadd, unsigned int padd, unsigned int fl
     return(0);
 }
 
+unsigned int mmu_small ( unsigned int vadd, unsigned int padd, unsigned int flags, unsigned int mmubase )
+{
+    unsigned int ra;
+    unsigned int rb;
+    unsigned int rc;
+
+    ra=vadd>>20;
+    rb=MMUTABLEBASE|(ra<<2);
+    rc=(mmubase&0xFFFFFC00)/*|(domain<<5)*/|1;
+    //hexstrings(rb); hexstring(rc);
+    PUT32(rb,rc); //first level descriptor
+    ra=(vadd>>12)&0xFF;
+    rb=(mmubase&0xFFFFFC00)|(ra<<2);
+    rc=(padd&0xFFFFF000)|(0xFF0)|flags|2;
+    //hexstrings(rb); hexstring(rc);
+    PUT32(rb,rc); //second level descriptor
+    return(0);
+}
+
 void pchar(unsigned char c)
 {
 	// Line status register.
@@ -78,6 +97,10 @@ unsigned int ra;
 		else
 			mmu_section(ra,ra,0x0000);
 		if(ra==0xFFF00000) break;
+	}
+	for(ra=0x700000;;ra+=0x00001000) {
+		mmu_small(ra,ra,0,0x00000400);
+		if (ra == 0x7ff000) break;
 	}
 
 	start_mmu(MMUTABLEBASE,0x00000001|0x1000|0x0004);
