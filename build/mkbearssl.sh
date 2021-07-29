71dfff888e8798017708db1c32e3248842775eea
Hiroki Mori <yamori813@yahoo.co.jp>
2021-07-29 10:54:55 +0900

    rename to ARMv4

--- build/mkbearssl.sh
+++ build/mkbearssl.sh
@@ -10,9 +10,9 @@ tar -zxf ${BEARSSL}.tar.gz
 
 START=`date '+%s'`
 
-cp -r ../BearSSL/MIPS4KEL.mk ${BEARSSL}/conf/
+cp -r ../BearSSL/ARMv4.mk ${BEARSSL}/conf/
 
-cd ${BEARSSL};patch -p1 < ../../BearSSL/bear.patch;make CONF=MIPS4KEL
+cd ${BEARSSL};patch -p1 < ../../BearSSL/bear.patch;make CONF=ARMv4
 
 END=`date '+%s'`
 
