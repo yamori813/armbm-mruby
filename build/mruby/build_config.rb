05ff211b0a45bb4ce3e7a93d730726a618129ca3
Hiroki Mori <yamori813@yahoo.co.jp>
2022-03-21 06:33:17 +0900

    change 3.0 define

--- build/mruby/build_config.rb
+++ build/mruby/build_config.rb
@@ -17,8 +17,8 @@ MRuby::CrossBuild.new('kendin') do |conf|
   conf.archiver.command = 'arm-none-eabi-ar'
 
   cc.defines << %w(MRB_METHOD_T_STRUCT)
-  cc.defines << %w(MRB_DISABLE_STDIO)
-  cc.defines << %w(MRB_WITHOUT_FLOAT)
+  cc.defines << %w(MRB_NO_STDIO)
+  cc.defines << %w(MRB_NO_FLOAT)
   cc.defines << %w(YABM_KEDEN)
   conf.cc.flags << "-mcpu=arm922t"
   conf.cc.include_paths = ["#{root}/include", "../build/work/newlib-3.0.0.20180831/newlib/libc/include"]
